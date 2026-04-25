b7b9b50a7f906c67bfd57f1e4185281663337e66
Darlimina <darli.mina01@unicatolica.edu.co>
2026-04-24 23:13:38 -0500

    cambios en el app.js

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const notFound     = require('./middlewares/notFound');
 
 const app = express();
-
+app.set('trust proxy', 1);
 // ==========================================
 // MIDDLEWARES DE SEGURIDAD
 // ==========================================
